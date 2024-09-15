91c1f82e21afcc5413b626567bcd2c571d8b653f
Kavya-76 <kavyabansal2000@gmail.com>
2024-09-15 23:36:28 +0530

    File Handling Setup

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,34 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from '../utils/ApiError.js'
+import { User } from "../models/user.model.js";
 
 const registerUser = asyncHandler( async (req,res)=>{
-    res.status(200).json({
-        message: "OK"
-    })
+    // get user details from frontend
+    // validation - not empty
+    // check if username already exists
+    // check for images and avatars
+    // upload them to cloudinary
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return response
+    const {username, email, fullName, password} = req.body;
+    
+    if(
+        [username, email, fullName, password].some((field)=>
+            field?.trim()==="")  // checks if the fiels is empty or not
+    ){
+        throw new ApiError(400, "all fields are required")
+    }
+
+    const existingUser = User.findOne({
+        $or: [{username}, {email}]  // checks if either or username or email already exists
+    })  
+    if(existingUser){
+        throw new ApiError(409, "User already exists")
+    }
+
+
 })
 
 export {registerUser}
