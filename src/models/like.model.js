74a1de167e2393c4adfd8dfae61aa48d72e8c1ed
Kavya-76 <kavyabansal2000@gmail.com>
2024-09-21 23:34:21 +0530

    Create comment, like, playlist, tweet models

---
+++ src/models/like.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const likeSchema = new mongoose.Schema({
+    video: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video"
+    },
+
+    comment: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
+    },
+
+    tweet: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tweet"
+    },
+
+    likedBy: {
+        type: mongoose.Schema.Tyeps.ObjectId,
+        ref: "User"
+    }
+}, {timestamps: true})
+
+likeSchema.plugin(mongooseAggregatePaginate);
+export const Like = mongoose.model("Like", likeSchema)
